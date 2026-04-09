6e1bd41ad0d37d6b7afca797b00cea16d747d801
Jeric-X <jericx@jericx.xyz>
2026-04-09 17:41:56 +0800

    feat: 支持用户在系统添加的根证书

--- plugins/withNetworkSecurityConfig.ts
+++ plugins/withNetworkSecurityConfig.ts
@@ -13,6 +13,7 @@ const NETWORK_SECURITY_CONFIG = `<?xml version="1.0" encoding="utf-8"?>
     <base-config cleartextTrafficPermitted="true">
         <trust-anchors>
             <certificates src="system" />
+            <certificates src="user" />
         </trust-anchors>
     </base-config>
 </network-security-config>
